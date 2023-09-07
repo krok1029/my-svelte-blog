a7b413f3dc7ddeb620bcbde961928e200039826b
leo lee <krok1029@gmail.com>
2023-09-07 20:34:29 +0800

    practice css 1

---
+++ src/routes/practices/css/1/+page.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import Box from './Box.svelte';
+	const boxesArray = new Array(60);
+</script>
+
+<div class="main">
+	<section>
+		<h2 class="text-xl">Scroll to Reveal</h2>
+	</section>
+
+	<div class="container">
+		{#each boxesArray as _box}
+			<Box />
+		{/each}
+	</div>
+</div>
+
+<style>
+	.main {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+		flex-direction: column;
+		background-color: #111;
+		overflow: hidden;
+	}
+	section {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		min-height: 100vh;
+	}
+	section h2 {
+		position: relative;
+		color: white;
+		font-size: 8vw;
+		font-weight: 500;
+	}
+	.container {
+		position: relative;
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr;
+		grid-gap: 30px;
+		width: 700px;
+		top: -200px;
+	}
+</style>
---
+++ src/routes/practices/css/1/Box.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	let scrollY: number;
+	let height: number;
+	let element: HTMLElement;
+
+	$: offset = element && element.offsetTop;
+	const random256 = () => Math.floor(Math.random() * 256);
+	const color = `rgb(${random256()}, ${random256()}, ${random256()})`;
+</script>
+
+<svelte:window bind:scrollY />
+
+<div
+	class="box"
+	style:background-color={color}
+	bind:this={element}
+	bind:clientHeight={height}
+	class:active={offset + 10 < scrollY}
+/>
+
+<style>
+	.box {
+		position: relative;
+		top: 50vh;
+		width: 200px;
+		height: 200px;
+		transition: 0.5s;
+		background-color: rgb(0, 0, 0);
+		border-radius: 10px;
+	}
+	.box:nth-child(3n + 1) {
+		transform: translate(-400px, 0) scale(0);
+	}
+	.box:nth-child(3n + 2) {
+		transform: translate(0, 0) scale(0);
+	}
+	.box:nth-child(3n + 3) {
+		transform: translate(400px, 0) scale(0);
+	}
+	.box.active {
+		transform: translate(0, 0) scale(1);
+	}
+</style>
