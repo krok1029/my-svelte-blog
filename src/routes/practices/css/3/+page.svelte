3084237ed68ab03a851055fc7428378cc111d53c
leo lee <krok1029@gmail.com>
2023-09-30 14:37:27 +0800

    update practice3

--- src/routes/practices/css/3/+page.svelte
+++ src/routes/practices/css/3/+page.svelte
@@ -1,20 +1,31 @@
 <script lang="ts">
+	let value0 = 0;
+	let value1 = 10;
+	let value2 = 65;
+	let value3 = 90;
+	const level = (value: number) => {
+		if (value > 80) {
+			return `#fd0a54`;
+		} else if (value > 60) {
+			return `#ffbc11`;
+		} else {
+			return `#0de5a8`;
+		}
+	};
+	const label = (value: number) => `${value}%`;
+	const items = ['HTML', 'HTML', 'CSS', 'Javascript'];
+	const values = [value0, value1, value2, value3];
 </script>
 
 <div class="body">
 	<div class="container">
-		<div class="box" style="--i:85%; --clr: #fd0a54">
-			<div class="circle"><h2>85<small>%</small></h2></div>
-			<h3>HTML</h3>
-		</div>
-		<div class="box" style="--i:70%; --clr: #ffbc11">
-			<div class="circle"><h2>70<small>%</small></h2></div>
-			<h3>CSS</h3>
-		</div>
-		<div class="box" style="--i:58%; --clr: #0de5a8">
-			<div class="circle"><h2>58<small>%</small></h2></div>
-			<h3>Javascript</h3>
-		</div>
+		{#each items as item, index}
+			<div class="box" style:--i={label(values[index])} style:--clr={level(values[index])}>
+				<input style="margin-bottom: 30px;" bind:value={values[index]} />
+				<div class="circle"><h2>{values[index]}<small>%</small></h2></div>
+				<h3>{item}</h3>
+			</div>
+		{/each}
 	</div>
 </div>
 
@@ -52,12 +63,19 @@
 		position: relative;
 		width: 150px;
 		height: 150px;
-		background: conic-gradient(
+		/* background: conic-gradient(
 			from 0deg,
 			var(--clr) 0%,
 			var(--clr) 0% var(--i),
 			#333 var(--i),
 			#333 100%
+		); */
+		background: conic-gradient(
+			from 0deg,
+			red 0%,
+			pink 30%,
+			blue 60%,
+			gray 100%
 		);
 		border-radius: 50%;
 
@@ -65,29 +83,29 @@
 		justify-content: center;
 		align-items: center;
 	}
-  .container .box .circle::before{
-    content: '';
-    position: absolute;
-    inset: 10px;
-    background-color: #1a252c;
-    border-radius: 50%;
-  }
-  .container .box h2 {
-    position: relative;
-    text-align: center;
-    font-size: 2.5em;
-    color: #fff;
-    font-weight: 600;
-  }
-  .container .box h2 small {
-    font-size: .5em;
-    font-weight: 300;
-  }
-  .container .box h3 {
-    color: #fff;
-    text-transform: uppercase;
-    margin-top: 20px;
-    font-weight: 500;
-    letter-spacing: 0.1em;
-  }
+	.container .box .circle::before {
+		content: '';
+		position: absolute;
+		inset: 10px;
+		background-color: #1a252c;
+		border-radius: 50%;
+	}
+	.container .box h2 {
+		position: relative;
+		text-align: center;
+		font-size: 2.5em;
+		color: #fff;
+		font-weight: 600;
+	}
+	.container .box h2 small {
+		font-size: 0.5em;
+		font-weight: 300;
+	}
+	.container .box h3 {
+		color: #fff;
+		text-transform: uppercase;
+		margin-top: 20px;
+		font-weight: 500;
+		letter-spacing: 0.1em;
+	}
 </style>
