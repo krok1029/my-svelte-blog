a11fb452d7d5d0436d61ced6b145fdf8fc840f78
leo lee <krok1029@gmail.com>
2024-03-10 16:11:45 +0800

    side project block

---
+++ src/components/card/SideProjectCard.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	export let path: string;
+	export let imgPath: string;
+	export let projecName: string;
+
+	const go = () => goto(path);
+</script>
+
+<div
+	class="rounded-xl shadow-lg px-4 py-3 flex bg-slate-700 flex-col hover:cursor-pointer border border-gray-200"
+	on:click={go}
+	on:keydown={() => {}}
+>
+	<img class="object-contain w-40 h-40 mx-auto" src={imgPath} alt={projecName} />
+
+	<div class="">
+		<a href={path} class="font-bold text-base mb-2 text-gray-100">{projecName}</a>
+	</div>
+</div>
+
+<style lang="postcss">
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,17 @@
 <!-- <h1 class="text-3xl font-bold underline">Hello world!</h1> -->
 <script lang="ts">
-	import { react, html5, css3, javascript, typescript, redux, storybook, webpack } from '$asset/logo/index';
+	import {
+		react,
+		html5,
+		css3,
+		javascript,
+		typescript,
+		redux,
+		storybook,
+		webpack
+	} from '$asset/logo/index';
 	import ImageBlock from '$components/ImageBlock.svelte';
+	import SideProjectCard from '$components/card/SideProjectCard.svelte';
 </script>
 
 <div class="container mx-auto p-4">
@@ -24,6 +34,31 @@
 			<ImageBlock src={redux} alt="redux" />
 			<ImageBlock src={storybook} alt="storybook" />
 			<ImageBlock src={webpack} alt="webpack" />
+		</div>
+	</section>
+	<section>
+		<h2 class="my-3">Side Project</h2>
+		<div class="flex gap-3 my-3">
+			<SideProjectCard
+				projecName="react snake game"
+				path="https://krok1029.github.io/react-snake-game/"
+				imgPath="https://github.com/krok1029/my-svelte-blog/blob/image/src/assets/image/%E6%88%AA%E5%9C%96%202024-03-10%20%E4%B8%8B%E5%8D%883.50.19.png?raw=true"
+			/>
+			<SideProjectCard
+				projecName="vue 新北 都更查詢地圖"
+				path="https://krok1029.github.io/xinbei-map/"
+				imgPath="https://github.com/krok1029/my-svelte-blog/blob/image/src/assets/image/%E6%88%AA%E5%9C%96%202024-03-10%20%E4%B8%8B%E5%8D%883.49.05.png?raw=true"
+			/>
+			<SideProjectCard
+				projecName="svelte-todos"
+				path="https://sveltekit-todos-3yz9a28on-leolees-projects.vercel.app/todos/"
+				imgPath="https://github.com/krok1029/my-svelte-blog/blob/image/src/assets/image/%E6%88%AA%E5%9C%96%202024-03-10%20%E4%B8%8B%E5%8D%883.47.38.png?raw=true"
+			/>
+			<SideProjectCard
+				projecName="fight-game"
+				path="https://fight-game-steel.vercel.app/"
+				imgPath="https://github.com/krok1029/my-svelte-blog/blob/image/src/assets/image/%E6%88%AA%E5%9C%96%202024-03-10%20%E4%B8%8B%E5%8D%883.44.51.png?raw=true"
+			/>
 		</div>
 	</section>
 </div>
