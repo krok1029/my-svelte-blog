d3d8352a517af74ce5fbf6432f658ecb1082a1a5
Leo Lee <krok1029@gmail.com>
2024-03-27 15:32:11 +0000

    get now

--- src/routes/practices/css/7/+page.svelte
+++ src/routes/practices/css/7/+page.svelte
@@ -1,3 +1,37 @@
-<div class="container bg-slate-600 h-screen w-screen"> 
-    <div></div>
+<script lang="ts">
+	let date = new Date();
+
+	setInterval(() => (date = new Date()), 1000);
+</script>
+
+<div class="container bg-slate-600 h-screen w-screen flex items-center justify-center gap-20">
+	<div class="section">
+		<span class="time">{date.getHours()}</span>
+		<span class="title">Hours</span>
+	</div>
+	<span class="colom">:</span>
+	<div class="section">
+		<span class="time">{date.getMinutes()}</span>
+		<span class="title">Minutes</span>
+	</div>
+	<span class="colom">:</span>
+	<div class="section">
+		<span class="time">{date.getSeconds()}</span>
+		<span class="title">Seconds</span>
+	</div>
 </div>
+
+<style lang="postcss">
+	.section {
+		@apply w-32 bg-sky-700 rounded-3xl flex flex-col items-center justify-center font-mono font-semibold text-white overflow-hidden;
+	}
+	.time {
+		@apply text-8xl p-2;
+	}
+	.title {
+		@apply text-2xl bg-emerald-900 w-full text-center p-3;
+	}
+	.colom {
+		@apply text-black text-5xl font-bold;
+	}
+</style>
